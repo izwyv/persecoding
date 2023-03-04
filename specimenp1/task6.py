28fd51ec7f4fe88c0e14feb35afb899e23657378
izwyv <125533439+izwyv@users.noreply.github.com>
2023-03-05 00:30:24 +0800

    Create task6.py

---
+++ specimenp1/task6.py
@@ -0,0 +1,16 @@
+first = input()
+second = input()
+third = input()
+fourth = input()
+fifth = input()
+
+if first == "Ellie":
+    print("1st")
+elif second == "Ellie":
+    print("2nd")
+elif third == "Ellie":
+    print("3rd")
+elif fourth == "Ellie":
+    print("4th")
+elif fifth == "Ellie":
+    print("5th")

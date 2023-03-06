4cf2784405182f49ffac16f207f3e99bc9ac3316
izwyv <125533439+izwyv@users.noreply.github.com>
2023-03-06 11:43:10 +0800

    Update task5.py

--- specimenp1/task5.py
+++ specimenp1/task5.py
@@ -12,3 +12,14 @@ else:
     exit()                    
 
 print(result)
+
+
+# or 
+
+number1 = int(input())
+sign = input()
+number2 = int(input())
+if sign == "*":
+    print(number1 * number2)
+elif sign == "+":
+    print(number1 + number2)
